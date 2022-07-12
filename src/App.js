25fa3e93de72d4279d3b946c8ebdc8956cdf49dc
Umidjon-Frontend <umidjonyusufovfrontend@gmail.com>
2022-07-12 16:25:31 +0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Loading from "./Components/Loading";
+import "./App.css";
+import Tours from "./Components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [tours, setTours] = useState([]);
+
+    const fetchTourData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setLoading(false);
+            setTours(data);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTourData();
+    }, []);
+
+    const deleteTour = (id) => {
+        const newFilterTour = tours.filter((tour) => tour.id !== id);
+        setTours(newFilterTour);
+    };
+
+    if (loading) {
+        return (
+            <main>
+                <Loading />
+            </main>
+        );
+    }
+
+    if (tours.length === 0) {
+        return (
+            <main>
+                <div className="title">
+                    <h2>no tours left</h2>
+                    <button className="btn" onClick={() => fetchTourData()}>
+                        refresh
+                    </button>
+                </div>
+            </main>
+        );
+    }
+
+    return (
+        <main>
+            <Tours tours={tours} deleteTour={deleteTour} />
+        </main>
+    );
 }
 
 export default App;
